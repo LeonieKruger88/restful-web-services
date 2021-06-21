f7e1696c6fdbe1627183c071c19d020eea5b765e
LeonieKruger88 <leonie.kruger88@gmail.com>
2021-06-21 14:56:56 +1200

    feat: Add method to UserDaoService to save new user

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Component
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
+    private static int usersCount=3;
 
     static {
         users.add(new User(1,"Adam",new Date()));
@@ -19,6 +20,15 @@ public class UserDaoService {
     public List<User> findAll(){
         return users;
     }
+
+    public User save(User user){
+        if (user.getId()==null){
+            user.setId(++usersCount);
+        }
+        users.add(user);
+        return user;
+    }
+
 
 
 
